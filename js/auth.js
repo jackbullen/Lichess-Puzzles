a724108389a6cac08f10501dc290c933761e5a47
Jack <37254717+jackbullen@users.noreply.github.com>
2023-05-31 14:44:33 -0700

    1st

---
+++ js/auth.js
@@ -0,0 +1,74 @@
+document.getElementById('authorizeButton').addEventListener('click', initiateAuthorization);
+
+// Following instructions from: https://lichess.org/api#tag/OAuth/operation/oauth
+
+// Step 1: Generate random strings
+function generateRandomString(length) {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      result += charset.charAt(randomIndex);
+    }
+    return result;
+  }
+  
+  // Step 2: Initiate authorization request
+  function initiateAuthorization() {
+    const codeVerifier = generateRandomString(32);
+    const state = generateRandomString(16);
+  
+    // Save codeVerifier and state in session storage
+    sessionStorage.setItem('codeVerifier', codeVerifier);
+    sessionStorage.setItem('state', state);
+  
+    // Redirect user to authorization endpoint
+    const redirectUri = 'https://lichess.org';
+    const authorizationUrl = `https://lichess.org/oauth?client_id=your-client-id&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&code_challenge_method=S256&code_challenge=${encodeURIComponent(codeVerifier)}&state=${encodeURIComponent(state)}`;
+    window.location.href = authorizationUrl;
+  }
+  
+  // Step 3: Handle authorization callback
+  function handleAuthorizationCallback() {
+    // Extract the returned query parameters from the URL
+    const params = new URLSearchParams(window.location.search);
+    const returnedCode = params.get('code');
+    const returnedState = params.get('state');
+  
+    // Retrieve the stored codeVerifier and state from session storage
+    const storedCodeVerifier = sessionStorage.getItem('codeVerifier');
+    const storedState = sessionStorage.getItem('state');
+  
+    // Verify that the returned state matches the stored state
+    if (returnedState !== storedState) {
+      console.error('State mismatch. Possible CSRF attack!');
+      return;
+    }
+  
+    // Perform the token exchange request using the returned authorization code
+    const tokenEndpoint = 'https://lichess.org/api/token';
+    const requestBody = {
+      grant_type: 'authorization_code',
+      code: returnedCode,
+      code_verifier: storedCodeVerifier,
+      redirect_uri: 'https://your-redirect-uri.com',
+      client_id: 'your-client-id'
+    };
+  
+    fetch(tokenEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams(requestBody)
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data containing the access token
+        console.log(data.access_token);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  
---
+++ js/script.js
@@ -0,0 +1,85 @@
+const form = document.getElementById("usernameForm");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  fetchPuzzleData();
+});
+  
+function fetchPuzzleData() {
+  const accessToken = "";
+  const puzzleCountInput = document.getElementById("puzzleCount");
+  const puzzleCount = puzzleCountInput.value;
+  const params = new URLSearchParams();
+  params.append("max", puzzleCount);
+  fetch(`https://lichess.org/api/puzzle/activity?${params.toString()}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+    .then(response => response.text())
+    .then(text => {
+      const trimmedText = text.trim();
+      console.log(trimmedText.split('\n'));
+  
+      const puzzles = trimmedText.split('\n').map(puzzleData => JSON.parse(puzzleData));
+  
+      const puzzlesContainer = document.getElementById("puzzlesContainer");
+      puzzlesContainer.innerHTML = ``;
+      puzzles.forEach((puzzle) => {
+        const fen = puzzle.puzzle.fen;
+        const puzzleId = puzzle.puzzle.id;
+        const result = puzzle.win ? "win" : "loss";
+        const rating = puzzle.rating;
+        const themes = puzzle.themes;
+        const lastMove = puzzle.lastMove;
+        if (result == "loss"){
+          createChessboard(puzzleId, fen, result);
+        }
+      });
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
+}
+
+function createChessboard(puzzleId, fen, result) {
+  
+  const puzzlesContainer = document.getElementById("puzzlesContainer");
+  
+
+  const puzzleContainer = document.createElement("div");
+  puzzleContainer.id = `puzzle-container`;
+
+  const puzzleLink = document.createElement("a");
+  puzzleLink.href = `https://lichess.org/training/${puzzleId}`; 
+  puzzleLink.target = "_blank"; 
+  puzzlesContainer.append(puzzleLink); 
+
+  puzzleLink.append(puzzleContainer)
+
+  const boardContainer = document.createElement("div");
+  boardContainer.classList.add("board-container");
+  puzzleContainer.appendChild(boardContainer);
+
+  const chessboard = document.createElement("div");
+  chessboard.id = `board_${puzzleId}`;
+  chessboard.classList.add("custom-chessboard");
+  boardContainer.appendChild(chessboard);
+
+
+  const boardResult = document.createElement("div");
+  boardResult.classList.add("board-result");
+  boardResult.innerText = `Puzzle ID: ${puzzleId}`;
+  puzzleContainer.appendChild(boardResult);
+
+  const fenParts = fen.split(" ");
+  const fenSideToMove = fenParts[1];
+  const orientation = fenSideToMove === "w" ? "white" : "black";
+
+  const board = ChessBoard(`board_${puzzleId}`, {
+    position: fen,
+    orientation: orientation
+  });
+}
+
+
+
